57525aec77509c017cd68240638009318d496d72
mintgreen1108 <mint@lanpangzideMacBook-Pro.local>
2017-10-12 16:27:52 +0800

    fix mvFile

--- code/mvFile.py
+++ code/mvFile.py
@@ -6,23 +6,38 @@ import os
 import shutil
 import datetime
 
-src = '/var/www/python_file/'
-dst = '/var/www/python_file3/'
-logPath = '/var/www/python_file/'
+src = '/datas/rtmp-server/video/'
+dst = '/tmp/rtmp-server/video/'
+logPath = '/datas/rtmp-server/'
 
 
 def writeLog(content):
+    'write move file exception'
     fobj = open(logPath + str(datetime.date.today()) + ".log", 'a')
     fobj.writelines(['%s \n' % content])
     fobj.close()
 
 
-# copy files
+def checkPath(path):
+    'if path not found,create'
+    if not os.path.exists(path):
+        os.makedirs(path, 7777)
+
+
+# move files
 try:
-    files = os.listdir(src)
-    for file in files:
-        if ((datetime.date.fromtimestamp(os.stat(src + file).st_ctime) - datetime.date.today()).days is 0):
-            shutil.move(src + file, dst + file)
+    checkPath(dst)
+    dicts = os.listdir(src)
+    for dict in dicts:
+        # source dictionary
+        spath = src + dict + '/'
+        # destination dictionary
+        dpath = dst + dict + '/'
+        files = os.listdir(spath)
+        for file in files:
+            if ((datetime.date.today() - datetime.date.fromtimestamp(os.stat(spath + file).st_ctime)).days > 7):
+                checkPath(dpath)
+                shutil.move(spath + file, dpath + file)
 except Exception, e:
     writeLog('move file error--' + str(e))
 
